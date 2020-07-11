ae36c1b822e5650f994b0c20f89146205f4f98f7
yinkaenoch <adedokunyinkaenoch@gmail.com>
2020-07-11 07:09:02 +0100

    [feat] refactor and style page header

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+// Hamburger
+document.querySelector("#nav-hamburger").addEventListener("click", () => {
+  document.querySelector(".secondary-nav").classList.toggle("show-sec-nav");
+});
+
+// Nav time
+document.querySelector("#nav-times").addEventListener("click", () => {
+  document.querySelector(".secondary-nav").classList.toggle("show-sec-nav");
+});
+
 // Automatically update copyright year
 document.querySelector(
   "#copyright-year"
